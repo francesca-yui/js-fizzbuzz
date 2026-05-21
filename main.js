0ffb5579e511e15c56b2e66017efc40d68fe7413
Francesca Yu <yui.francesca@gmail.com>
2026-05-21 12:30:16 +0200

    const added at the beginning

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
+const multiplyThree = 'Fizz';
+const multiplyFive = 'Buzz';
+const multiplyBoth = 'FizzBuzz'
+
 // numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
 
 //Stampa FizzBuzz per multipli di 3 e 5 (i % 3 === 0 && i % 5 === 0)
     if (i % 15 === 0) {
-        console.log('FizzBuzz')
+        console.log(multiplyBoth)
     }  
 
 // Altrimenti stampa Fizz per i multipli di 3
     else if (i % 3 === 0) {
-        console.log('Fizz');
+        console.log(multiplyThree);
     }
         
 // Altrimenti stampa Buzz per i multipli di 5
     else if (i % 5 === 0) {
-        console.log('Buzz');
+        console.log(multiplyFive);
     }
 
 // Altrimenti stampa il numero normale
